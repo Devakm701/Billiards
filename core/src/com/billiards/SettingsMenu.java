6e245870a7945f67800917d4e68a707d00fc6b68
Jeffrey Liu <bobfredthegreat@gmail.com>
2022-05-11 14:36:10 -0700

    Co-authored-by: Hopeyro <Hopeyro@users.noreply.github.com>

--- core/src/com/billiards/SettingsMenu.java
+++ core/src/com/billiards/SettingsMenu.java
@@ -6,23 +6,31 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class SettingsMenu implements Screen {
-    Preferences settings = Gdx.app.getPreferences("8 Ball Pool");
-    Billiards billiardsGame;
-    Stage stage;
-    Image background;
-    BitmapFont font = new BitmapFont();
+    private Preferences settings = Gdx.app.getPreferences("8 Ball Pool");
+    private Billiards billiardsGame;
+    private Stage stage;
+    private Image background;
+    private BitmapFont font = new BitmapFont();
+    private TextButton hiGraphicsButton;
+    private TextButton loGraphicsButton;
     
 
     public SettingsMenu(Billiards game, Texture bg) { 
         billiardsGame = game;
+        
         background = new Image(bg);
     }
 
@@ -46,7 +54,31 @@ public class SettingsMenu implements Screen {
 
         // Table 
         Table table = new Table();
+
+        // Button Styler
+        TextButtonStyle style = new TextButtonStyle();
+        style.font = font;
+        style.up = new TextureRegionDrawable(new TextureRegion(new Texture("blue.png"))); // up is when button released
+        style.down = new TextureRegionDrawable(new TextureRegion(new Texture("blueHover.png"))); // down is pressed
+        style.over = new TextureRegionDrawable(new TextureRegion(new Texture("bluePress.png"))); 
         
+        // High Graphics
+        hiGraphicsButton = new TextButton("High", style);
+        hiGraphicsButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                billiardsGame.closeMenu();
+            }
+        });
+
+        // Low Graphics
+        loGraphicsButton = new TextButton("Low", style);
+        loGraphicsButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                billiardsGame.closeMenu();
+            }
+        });
 
     }
 
