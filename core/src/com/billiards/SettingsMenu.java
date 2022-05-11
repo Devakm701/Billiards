f357747e0373fa6812872a15115033aaa98cb5fa
Jeffrey Liu <bobfredthegreat@gmail.com>
2022-05-11 11:47:41 -0700

    hello

--- core/src/com/billiards/SettingsMenu.java
+++ core/src/com/billiards/SettingsMenu.java
@@ -3,10 +3,14 @@ package com.billiards;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 
 public class SettingsMenu implements Screen {
@@ -28,7 +32,22 @@ public class SettingsMenu implements Screen {
         stage.addActor(background);
         background.setPosition(0f, 0f);
         Gdx.input.setInputProcessor(stage);
-        font = new BitmapFont(); 
+        
+        // Initialize Font
+        font = new BitmapFont();
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Comic_Sans_MS.ttf")); // Oh no 
+        FreeTypeFontParameter fontParam = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParam.size = 16;
+        fontParam.borderWidth = 1;
+        fontParam.borderColor = Color.BLACK;
+        fontParam.color = Color.WHITE;
+        font = fontGenerator.generateFont(fontParam);
+        fontGenerator.dispose(); 
+
+        // Table 
+        Table table = new Table();
+        
+
     }
 
     @Override
